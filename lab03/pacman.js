b5ff86cdc057f7ce0cbde318a2f2e10eb0435319
jayboots <84293704+jayboots@users.noreply.github.com>
2024-06-07 01:38:13 -0400

    feat(lab 03): :sparkles: move left and right, eat pellets

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -3,11 +3,10 @@
 /**
  * Creates an array representing a pacman game of size N (>= 5), and a dictionary tracking the indices of key pieces
  *
- * @param {*} N
- * @returns {{}}
+ * @param {*} N - size of the game board
+ * @returns {{}} - game variables in an array
  */
 function createGame(N){
-    
     const pellet = "."
 
     // Require a minimum of 5 things to do
@@ -35,7 +34,105 @@ function createGame(N){
         pieces[key] = random;
     }
 
-    return [pelletLine, pieces]
+    return [pelletLine, pieces, N-1]
 }
 
-console.log(createGame(10))
+// Establish a game
+// var size = Math.floor(Math.random() * 30);
+var size = 10;
+console.log("Creating game of size " + size)
+game = createGame(size)
+console.log(game[0])
+
+function moveLeft(game){
+    let gameBoard = game[0];
+    let positions = game[1];
+    let size = game[2];
+    // console.log(gameBoard);
+    let pacPos = positions["C"];
+    targetPos = pacPos;
+    // console.log("Pacman at " + pacPos + ", moving left");
+
+    // Target tile index
+    //Modify this to determine what pacman should be going for
+    if (pacPos == 0){
+        targetPos = size
+    }
+    else{
+        targetPos -= 1
+    }
+
+    // Evaluate the target tile
+    activeTile = gameBoard[targetPos]
+    switch(activeTile) {
+        case ".":
+            // console.log(positions)
+            // console.log(targetPos + " is a pellet");
+            gameBoard[targetPos] = "C"; //eat pellet
+            gameBoard[pacPos] = " "; //empty space behind
+            positions["C"] = targetPos;
+            console.log(gameBoard)
+            // console.log(positions)
+            return moveLeft([gameBoard, positions, size])
+        case "^":
+            // console.log(targetPos + " is a ghost")
+            console.log("Pacman walked into a ghost, so game over.")
+            break;
+        case "@":
+            gameBoard[targetPos] = "C";
+            gameBoard[pacPos] = " ";
+            positions["C"] = targetPos;
+            console.log(gameBoard)
+            // console.log(positions)
+            return moveLeft([gameBoard, positions, size])
+        default: //free space, can move through without issue
+            gameBoard[targetPos] = "C";
+            // gameBoard[pacPos] = " ";
+            positions["C"] = targetPos;
+            console.log(gameBoard)
+            return moveLeft([gameBoard, positions, size])
+    }
+}
+
+function moveRight(game){
+    let gameBoard = game[0];
+    let positions = game[1];
+    let size = game[2];
+    let pacPos = positions["C"];
+    targetPos = pacPos;
+
+    // Target tile index
+    //Modify this to determine what pacman should be going for
+    if (pacPos == size){
+        targetPos = 0
+    }
+    else{
+        targetPos += 1
+    }
+
+    // Evaluate the target tile
+    activeTile = gameBoard[targetPos]
+    switch(activeTile) {
+        case ".":
+            gameBoard[targetPos] = "C"; //eat pellet
+            gameBoard[pacPos] = " "; //empty space behind
+            positions["C"] = targetPos;
+            console.log(gameBoard)
+            return moveRight([gameBoard, positions, size])
+        case "^":
+            console.log("Pacman walked into a ghost, so game over.")
+            break;
+        case "@": //Some effect of the fruit here
+            gameBoard[targetPos] = "C";
+            gameBoard[pacPos] = " ";
+            positions["C"] = targetPos;
+            console.log(gameBoard)
+            return moveRight([gameBoard, positions, size])
+        default:
+            console.log(targetPos + " is a free space")
+            break;
+    }
+}
+
+moveLeft(game)
+moveRight(game)
