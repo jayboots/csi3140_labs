53a36a09bb064aedb951c774948523eb03660118
jayboots <84293704+jayboots@users.noreply.github.com>
2024-07-23 23:51:17 -0400

    feat(lab 10): :sparkles: add animation

--- lab10/player.js
+++ lab10/player.js
@@ -14,14 +14,18 @@ export class Player {
         this.image = document.getElementById('player');
         this.speed = 0;
         this.frameX = 0;
-        this.frameY = 0;
+        this.maxFrame = 2;
+        this.fps = 10;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
+        this.frameY = 1;
         this.maxSpeed = 20;
         // this.states = [new Standing(this), new Running(this), new Jumping(this)];
         // this.currentState = this.states[0];
         // this.currentState.enter();
     }
 
-    update(input){
+    update(input, deltaTime){
         // this.currentState.handleInput(input);
         this.y += this.velocity
         if (input.includes(" ") || input.includes("ArrowUp") && this.onGround()){
@@ -41,6 +45,21 @@ export class Player {
             this.y = this.game.height - this.height
         }
         // console.log(this.y)
+
+        // Animation?
+        if (this.frameTimer > this.frameInterval){
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame){
+                this.frameX += 1;
+            }
+            else {
+                this.frameX = 0;
+            }
+        }
+        else{
+            this.frameTimer += deltaTime;
+        }
+
 
     }
     // Draw Frames
